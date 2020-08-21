8d0d5b73f3d8b38496f946cc74820355271c1563
RohanAlex <rohanalex2002@gmail.com>
2020-08-21 10:22:56 +0530

    Basic Math
    To find sum, difference, product and quotient of two numbers.

---
+++ basic maths.py
@@ -0,0 +1,10 @@
+a=int(input('enter first number = '))
+b=int(input("enter second number = "))
+dif=a-b
+sum=a+b
+product=a*b
+k=a/b
+print("sum=",int(sum))
+print("difference=",int(dif))
+print("product=",int(product))
+print('divide=', int(k))
